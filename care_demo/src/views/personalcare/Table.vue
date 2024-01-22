a9d0b6299df1cc4486fbaf558e1c49994addf56d
RatchanonPh <107181147+RatchanonPh@users.noreply.github.com>
2024-01-23 01:41:09 +0700

    create project

---
+++ care_demo/src/views/personalcare/Table.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>persnal care</h1>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'table-persnalcare'
+    
+}
+</script>
+<style lang="">
+    
+</style>
